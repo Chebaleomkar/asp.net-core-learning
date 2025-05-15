56d538cc8a9afb7edf03d89307d57ee1f03737fc
Omkar Chebale <omkarchebale0@gmail.com>
2025-05-16 00:27:29 +0530

    - define data models

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using GameStoreApi.Data;
 using GameStoreApi.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var  connectionString = "Data source=GameStore.db";
+
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+
 
 app.MapGet("/", () => "Hello World!");
 
