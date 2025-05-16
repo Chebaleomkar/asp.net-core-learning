f6ef5b480c0323275364d36fbe9f5cb498b5c93f
Omkar Chebale <omkarchebale0@gmail.com>
2025-05-16 22:24:34 +0530

    data seeding and upgrading sqlite package to version 9

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -3,9 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameStoreApi.Data;
 
-public class GameStoreContext(DbContextOptions<GameStoreContext> options) :DbContext(options)
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
 {
-    public DbSet<Game> Games  => Set<Game>();
+    public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Adventure" },
+            new { Id = 3, Name = "RPG" },
+            new { Id = 4, Name = "Role Playing" },
+            new { Id = 5, Name = "Shooter" }
+        );
+    }
 };
